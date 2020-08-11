0bed7f723ebecfb8b047e3c75c3332d0c0fdb3cb
Arul <arul@luracast.com>
2020-08-11 19:10:16 +0800

    Cleans up and Fixed model make command

--- bootstrap/Console/ModelMakeCommand.php
+++ bootstrap/Console/ModelMakeCommand.php
@@ -46,10 +46,8 @@ class ModelMakeCommand extends GeneratorCommand
 
         if ($this->option('all')) {
             $this->input->setOption('factory', true);
-            $this->input->setOption('seed', true);
             $this->input->setOption('migration', true);
-            $this->input->setOption('controller', true);
-            $this->input->setOption('resource', true);
+            $this->input->setOption('seed', true);
         }
 
         if ($this->option('factory')) {
@@ -62,10 +60,6 @@ class ModelMakeCommand extends GeneratorCommand
 
         if ($this->option('seed')) {
             $this->createSeeder();
-        }
-
-        if ($this->option('controller') || $this->option('resource') || $this->option('api')) {
-            $this->createController();
         }
     }
 
@@ -86,7 +80,12 @@ class ModelMakeCommand extends GeneratorCommand
      */
     protected function buildReplacements(array $replace)
     {
-        $table = $this->option('table') ?? str_plural(strtolower(preg_replace('/([a-z])([A-Z])/', '$1_$2', $this->argument('name'))));
+        $table = $this->option('table')
+            ?? str_plural(strtolower(
+                preg_replace('/([a-z])([A-Z])/',
+                    '$1_$2',
+                    class_basename($this->argument('name')
+                    ))));
         $fields = Schema::getColumnListing($table);
 
         $hide = ['password', 'secret'];
@@ -140,94 +139,6 @@ class ModelMakeCommand extends GeneratorCommand
         ]);
     }
 
-    /**
-     * Format the command class stub.
-     *
-     * @param string $stub
-     *
-     * @return string
-     */
-    protected function formatStub($stub)
-    {
-        $className = $this->input->getArgument('name');
-        $tableName = is_null($this->option('table'))
-            ? str_plural(strtolower(preg_replace('/([a-z])([A-Z])/', '$1_$2', $className)))
-            : $this->option('table');
-        $fields = Schema::getColumnListing($tableName);
-
-        $hide = ['password', 'secret'];
-        $avoid = ['id', 'ID', 'verified', 'active'];
-
-        $timestamps = 'true';
-        $fillable = '';
-        $hidden = '';
-        $import = '';
-        $use = '';
-        $dates = '';
-
-        $properties = '';
-        if (!empty($fields)) {
-            foreach ($fields as $property) {
-                if ($isDate = ends_with($property, '_at')) {
-                    $avoid[] = $property;
-                }
-                if ($is_id = ends_with($property, '_id')) {
-                    $avoid[] = $property;
-                }
-                //$type = DB::connection()->getDoctrineColumn($tableName, $property)->getType()->getName();
-                list($type, $subType) = $this->guessType($property);
-                $pt = in_array($property, $hide)
-                    ? '@property-write' : (
-                    in_array($property, $avoid) || $isDate ? '@property-read ' : '@property      '
-                    );
-                $properties .= "$pt $type \${$property}$subType\n * ";
-            }
-
-            $timestamps = in_array('created_at', $fields) ? 'true' : 'false';
-            $fields = array_diff($fields, $avoid);
-            $hide = array_intersect($fields, $hide);
-
-            $fillable = "'" . implode("',\n        '", $fields) . "'";
-
-            if (in_array('deleted_at', $fields)) {
-                $import = "use Illuminate\\Database\\Eloquent\\SoftDeletingTrait;\n\n";
-                $use = "use SoftDeletingTrait;\n\n    protected \$dates = ['deleted_at'];\n        ";
-            }
-        }
-
-        if (!empty($hide)) {
-            $hidden = "'" . implode("',\n        '", $hide) . "'";
-        }
-
-        $stub = str_replace(
-            [
-                'DummyNamespace',
-                'class:name',
-                'table:name',
-                'table:timestamps',
-                'table:fillable',
-                'table:hidden',
-                'softdelete:import',
-                'softdelete:use',
-                'comment:properties'
-            ],
-            [
-                rtrim(getAppNamespace(), '\\'),
-                $className,
-                $tableName,
-                $timestamps,
-                $fillable,
-                $hidden,
-                $import,
-                $use,
-                $properties
-            ],
-            $stub
-        );
-
-        return $stub;
-    }
-
     protected function guessType($name)
     {
         $subtype = '';
@@ -273,8 +184,6 @@ class ModelMakeCommand extends GeneratorCommand
             ['migration', 'm', InputOption::VALUE_NONE, 'Create a new migration file for the model'],
             ['seed', 's', InputOption::VALUE_NONE, 'Create a new seeder file for the model'],
             ['pivot', 'p', InputOption::VALUE_NONE, 'Indicates if the generated model should be a custom intermediate table model'],
-            ['resource', 'r', InputOption::VALUE_NONE, 'Indicates if the generated controller should be a resource controller'],
-            ['api', null, InputOption::VALUE_NONE, 'Indicates if the generated controller should be an API controller'],
         ];
     }
 }
